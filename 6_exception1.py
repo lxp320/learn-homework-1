a63b84cc348d4300bfa9d50859b31cee362914f6
Lxp320 <Lxp320pae94@gmail.com>
2023-11-23 22:07:29 +0300

    Сделал шестое задание

--- 6_exception1.py
+++ 6_exception1.py
@@ -9,12 +9,23 @@
   и завершала работу при помощи оператора break
     
 """
-
+# Функция вопрос)
 def hello_user():
     """
     Замените pass на ваш код
     """
-    pass
+    # Бесконечный цикл
+    while True:
+        # Попытка принять пользовательский ввод
+        try:
+            input('Как дела?\n')
+        # Перехват KeyboardInterrupt (Ctrl + C)
+        except KeyboardInterrupt:
+            # Вывод прощания
+            print("Пока!")
+            # Выход из бесконечного цикла
+            break
+
     
 if __name__ == "__main__":
     hello_user()
